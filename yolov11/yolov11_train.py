5cd9045a7a12244513ef43c4debc180010ee355e
cafechen <chenjian945@gmail.com>
2025-06-13 17:34:36 +0800

    Adjusting train parameters

--- yolov11/yolov11_train.py
+++ yolov11/yolov11_train.py
@@ -8,11 +8,11 @@ train_results = model.train(
     data="/home/steven/dataset/data.yaml",  # Path to dataset configuration file
     epochs=20,  # Number of training epochs
     imgsz=640,  # Image size for training
-    device=[0],  # Device to run on (e.g., 'cpu', 0, [0,1,2,3])
-    workers=0, # Number of worker threads for data loading. Higher values can speed up data preprocessing but may increase CPU usage. Setting to 0 uses main thread, which can be more stable in some environments.
-    batch=32, # Specifies export model batch inference size or the maximum number of images the exported model will process concurrently in predict mode. For Edge TPU exports, this is automatically set to 1.
+    device=[0, 1],  # Device to run on (e.g., 'cpu', 0, [0,1,2,3])
+    workers=8, # Number of worker threads for data loading. Higher values can speed up data preprocessing but may increase CPU usage. Setting to 0 uses main thread, which can be more stable in some environments.
+    batch=64, # Specifies export model batch inference size or the maximum number of images the exported model will process concurrently in predict mode. For Edge TPU exports, this is automatically set to 1.
     cache=True, # Enables caching of dataset images in memory (True/ram), on disk (disk), or disables it (False). Improves training speed by reducing disk I/O at the cost of increased memory usage.
     save_period=10, # Frequency of saving model checkpoints, specified in epochs. A value of -1 disables this feature. Useful for saving interim models during long training sessions.
     project='/home/steven/training_output',  # Name of the project directory where training outputs are saved. Allows for organized storage of different experiments.
-    name="yolo11l_sf6"  # Name of the training run. Used for creating a subdirectory within the project folder, where training logs and outputs are stored.
+    name="yolo11l_sf6", # Name of the training run. Used for creating a subdirectory within the project folder, where training logs and outputs are stored.
 )
