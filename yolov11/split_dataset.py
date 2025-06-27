c05f0f2bbdab690f6808c8111dbd41de40cdca74
cafechen <chenjian945@gmail.com>
2025-06-27 11:31:01 +0800

    Split the dataset based on the labels.

--- yolov11/split_dataset.py
+++ yolov11/split_dataset.py
@@ -11,16 +11,18 @@ dataset/                                dataset/
 ├── labels/                             │   ├── train/
 │   ├── a.txt                           │   ├── val/
 │   ├── b.txt          
-│   └── ...            
+│   └── ...            06.10
 '''
 
 # Configuration
-image_dir = 'D:\sf6\lb\images'  # Path to images
-label_dir = 'D:\df6\lb\labels'  # Path to labels
+image_dir = '/Users/steven/sf6/bg_1364780_20250610_154512/images'  # Path to images
+label_dir = '/Users/steven/sf6/bg_1364780_20250610_154512/labels'  # Path to labels
+image_target_dir = '/Users/steven/sf6/dataset/images'  # Path to images
+label_target_dir = '/Users/steven/sf6/dataset/labels'  # Path to labels
 train_ratio = 0.8  # 80% training, 20% validation
 
 # Get all file names without extensions
-filenames = [os.path.splitext(f)[0] for f in os.listdir(image_dir) if f.endswith(('.jpg', '.png', '.jpeg'))]
+filenames = [os.path.splitext(f)[0] for f in os.listdir(label_dir) if f.endswith('.txt') and f != 'classes.txt']
 random.shuffle(filenames)
 
 # Split into train and val
@@ -30,8 +32,8 @@ val_files = filenames[train_size:]
 
 # Create destination directories
 for subdir in ['train', 'val']:
-    os.makedirs(os.path.join(image_dir, subdir), exist_ok=True)
-    os.makedirs(os.path.join(label_dir, subdir), exist_ok=True)
+    os.makedirs(os.path.join(image_target_dir, subdir), exist_ok=True)
+    os.makedirs(os.path.join(label_target_dir, subdir), exist_ok=True)
 
 
 # Move image and label files
@@ -41,11 +43,11 @@ def move_files(file_list, target):
         for ext in img_exts:
             src_img = os.path.join(image_dir, name + ext)
             if os.path.exists(src_img):
-                shutil.move(src_img, os.path.join(image_dir, target, name + ext))
+                shutil.copy(src_img, os.path.join(image_target_dir, target, name + ext))
                 break
         src_label = os.path.join(label_dir, name + '.txt')
         if os.path.exists(src_label):
-            shutil.move(src_label, os.path.join(label_dir, target, name + '.txt'))
+            shutil.copy(src_label, os.path.join(label_target_dir, target, name + '.txt'))
 
 
 # Perform file moving
